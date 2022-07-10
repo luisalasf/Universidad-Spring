1947a4a512be7230b29a3fbde8b5586d34ff48b9
Luis Salas <luispiyu007@gmail.com>
2022-07-10 17:42:35 -0500

    Agrega codigo de crud en servicios

--- ControlClientes/src/main/java/mx/com/gm/service/PersonaServiceImpl.java
+++ ControlClientes/src/main/java/mx/com/gm/service/PersonaServiceImpl.java
@@ -1,4 +1,36 @@
 package mx.com.gm.service;
 
-public class PersonaServiceImpl {
+import mx.com.gm.dao.PersonaDao;
+import mx.com.gm.domain.Persona;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PersonaServiceImpl implements PersonaService{
+    @Autowired
+    private PersonaDao personaDao;
+
+    @Override
+    public void guardar(Persona persona) {
+        personaDao.save(persona);
+    }
+
+    @Override
+    public Persona encontrarPersona(Persona persona) {
+        //Si no encuentra a la persona, regresa null
+        //regresa un Optional<Persona>
+        return personaDao.findById(persona.getId_persona()).orElse(null);
+    }
+
+    @Override
+    public List<Persona> listarPersonas() {
+        return (List<Persona>) personaDao.findAll(); //regresa un Iterable, por lo que se castea a List
+    }
+
+    @Override
+    public void eliminar(Persona persona) {
+        personaDao.delete(persona);
+    }
 }
